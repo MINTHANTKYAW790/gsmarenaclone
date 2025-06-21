db430af23f86ebbcfb98895921ba9aa3b97d1f93
UAB\minthantkyaw <minthantkyaw@uab.com.mm>
2025-06-21 12:42:43 +0630

    comment out the faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker::create();
+        // $faker = Faker::create();
 
         // Seed Brands
         $brands = [
